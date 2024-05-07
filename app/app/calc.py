d67868e273d55aeeee64be24e1693e78f38cf8c5
Daniel Samuel <dansam5000@gmail.com>
2024-05-07 08:15:27 +0200

    Add db services with docker compose

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculation functions
 def add(x, y):
 	"""Add two numbers and return the result"""
 	return x + y
+
+def subtract(x, y):
+	"""Subtract x from y and return the result"""
+	return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,10 @@ class CalcTests(SimpleTestCase):
 		result = calc.add(3, 8)
 
 		self.assertEqual(result, 11)
+
+
+	def test_subtract_numbers(self):
+		"""Test that values are subtracted and returned"""
+		result = calc.subtract(5, 11)
+
+		self.assertEqual(result, 6)
