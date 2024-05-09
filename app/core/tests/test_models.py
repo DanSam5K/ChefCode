ab88d6d91dd1c37d45c798a954da8f2f4e1b59d7
Daniel Samuel <dansam5000@gmail.com>
2024-05-09 17:04:41 +0200

    test empty email raises value error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,3 +33,8 @@ class ModelTests(TestCase):
 		for email, expected in sample_emails:
 			user = get_user_model().objects.create_user(email, 'test123')
 			self.assertEqual(user.email, expected)
+
+	def test_new_user_without_email_raises_error(self):
+		"""Test creating user without email raises value error"""
+		with self.assertRaises(ValueError):
+			get_user_model().objects.create_user(None, 'test123')
