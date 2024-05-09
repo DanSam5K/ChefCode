ed35c1a64cce737f85b8ba736d8bf6b120f049fb
Daniel Samuel <dansam5000@gmail.com>
2024-05-09 17:53:10 +0200

    test & implement support for creating superuser

--- app/core/models.py
+++ app/core/models.py
@@ -3,30 +3,39 @@ Database models.
 """
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-	PermissionsMixin
+    PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-	"""Manager for user profiles."""
+    """Manager for user profiles."""
 
-	def create_user(self, email, password=None, **extra_fields):
-		"""Create, save and return a new user."""
-		if not email:
-			raise ValueError('Users must have an email address')
-		user = self.model(email=self.normalize_email(email), **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-		return user
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, save and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-	"""User in the system."""
-	email = models.EmailField(max_length=255, unique=True)
-	name = models.CharField(max_length=255)
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-	objects = UserManager()
+    objects = UserManager()
 
-	USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
